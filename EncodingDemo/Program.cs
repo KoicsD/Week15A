506d16ddcbb81f4381a032059fd5073675cff082
Daniel Koics <koics.dani@gmail.com>
2016-04-26 11:03:14 +0200

    Solve Encoding task
    Boot.ini does not exists, \Windows\WindowsUpdate.log is used instead
    Unfortunatelly, Notepad can display even UTF-7 text properly.

--- EncodingDemo/Program.cs
+++ EncodingDemo/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace EncodingDemo
 {
     class Program
     {
+        static string source = "C:\\Windows\\WindowsUpdate.log";
+        static string dest = "WindowsUpdateutf7.txt";
+
         static void Main(string[] args)
         {
+            using (StreamReader reader = new StreamReader(source))
+            {
+                using (StreamWriter writer = new StreamWriter(dest, false, Encoding.UTF7))
+                {
+                    writer.Write(reader.ReadToEnd());
+                }
+            }
         }
     }
 }
